c760a01b5c571bc3ad6f58aeb8a51a16961ce245
Jens Kromdijk <jenskromdijk@gmail.com>
2025-11-14 08:53:34 +0000

    Work on implementing animator class

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 
 #include "src/engine.hpp"
 #include "src/ibl.hpp"
+#include "src/bones.hpp"
 
 int main()
 {
@@ -13,8 +14,8 @@ int main()
     Engine engine{};
     if (!engine.init(640, 480, "OpenGL Window"))
     {
-        std::cout << "Failed to initialize engine!\n";
-        return 1;
+	std::cout << "Failed to initialize engine!\n";
+	return 1;
     }
 
     std::cout << "Initialized engine!\n";
@@ -24,8 +25,9 @@ int main()
     engine.addModel("cube", "data/models/monkey.glb");
     engine.addModel("light", "data/models/spartan.glb");
 
-    const Model* light{engine.getModel("light")};
-
+    Model* light{engine.getModel("light")};
+    BoneAnimation spartanAnimation {"data/models/spartan.glb", light};
+    BoneAnimator spartanAnimator{&spartanAnimation};
     // engine.enableWireframe();
     const std::vector<glm::vec3> spheres{{1.f, 4.f, 2.f}};
 
@@ -41,44 +43,47 @@ int main()
     glViewport(0, 0, engine.getWidth(), engine.getHeight());
     while (!engine.getQuit())
     {
-        engine.enablePostProcessing();
-        // clear screen
-        engine.clear();
+	// update game state
+	spartanAnimator.updateAnimation(engine.getDeltaTime());
 
-        engine.useShader("texturePBR");
-        engine.setVec3("viewPos", engine.getCameraPosition(), "texturePBR");
+	// do rendering
+	engine.enablePostProcessing();
+	// clear screen
+	engine.clear();
 
-        glm::mat4 model{1.0f};
-        for (std::size_t i{0}; i < spheres.size(); ++i)
-        {
-            model = glm::mat4{1.0f};
-            model = glm::scale(model, glm::vec3{0.2f});
-            model = glm::translate(model, spheres[i]);
-            // model = glm::rotate(model, engine.getTime() * 0.6f, {0.0f, 1.0f, 0.0f});
-            engine.setMat4("model", model, "texturePBR");
-            engine.setMat4("view", engine.getViewMatrix(), "texturePBR");
-            engine.setMat4("projection", engine.getProjectionMatrix(), "texturePBR");
-            engine.setMat3("normalMat", engine.getNormalMatrix(model), "texturePBR");
-            engine.setInt("irradianceMap", 10, "texturePBR");
-            glActiveTexture(GL_TEXTURE10);
-            glBindTexture(GL_TEXTURE_CUBE_MAP, iblGenerator.getIrradianceMap());
-            engine.setInt("prefilterMap", 11, "texturePBR");
-            glActiveTexture(GL_TEXTURE11);
-            glBindTexture(GL_TEXTURE_CUBE_MAP, iblGenerator.getPrefilterMap());
-            engine.setInt("brdfLUT", 12, "texturePBR");
-            glActiveTexture(GL_TEXTURE12);
-            glBindTexture(GL_TEXTURE_2D, iblGenerator.getBRDFLutMap());
-            light->renderPBR(engine.getShader("texturePBR"));
-        }
+	engine.useShader("texturePBR");
+	engine.setVec3("viewPos", engine.getCameraPosition(), "texturePBR");
 
-        iblGenerator.renderSkybox(&engine);
+	const std::vector<glm::mat4>& transforms {spartanAnimator.getFinalBoneMatrices()};
+	for (std::size_t i{0}; i < transforms.size(); ++i)
+	    engine.setMat4("finalBonesMatrices[" + std::to_string(i) + "]", transforms[i], "texturePBR");
 
-        engine.disablePostProcessing();
-        engine.renderPostProcessing();
+	glm::mat4 model {glm::mat4{1.0f}};
+	model = glm::scale(model, glm::vec3{0.2f});
+	// model = glm::rotate(model, engine.getTime() * 0.6f, {0.0f, 1.0f, 0.0f});
+	engine.setMat4("model", model, "texturePBR");
+	engine.setMat4("view", engine.getViewMatrix(), "texturePBR");
+	engine.setMat4("projection", engine.getProjectionMatrix(), "texturePBR");
+	engine.setMat3("normalMat", engine.getNormalMatrix(model), "texturePBR");
+	engine.setInt("irradianceMap", 10, "texturePBR");
+	glActiveTexture(GL_TEXTURE10);
+	glBindTexture(GL_TEXTURE_CUBE_MAP, iblGenerator.getIrradianceMap());
+	engine.setInt("prefilterMap", 11, "texturePBR");
+	glActiveTexture(GL_TEXTURE11);
+	glBindTexture(GL_TEXTURE_CUBE_MAP, iblGenerator.getPrefilterMap());
+	engine.setInt("brdfLUT", 12, "texturePBR");
+	glActiveTexture(GL_TEXTURE12);
+	glBindTexture(GL_TEXTURE_2D, iblGenerator.getBRDFLutMap());
+	light->renderPBR(engine.getShader("texturePBR"));
 
-        // update engine
-        engine.displayFrameTime();
-        engine.update();
+	iblGenerator.renderSkybox(&engine);
+
+	engine.disablePostProcessing();
+	engine.renderPostProcessing();
+
+	// update engine
+	engine.displayFrameTime();
+	engine.update();
     }
 
     return 0;
