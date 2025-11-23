3f4be335137bab1318c67a3dc99653b57b87cf52
Jens Kromdijk <jenskromdijk@gmail.com>
2025-11-23 14:01:54 +0000

    Start work on deferred renderer

---
+++ src/renderer.cpp
@@ -0,0 +1,66 @@
+//
+// Created by jenskromdijk on 23/11/2025.
+//
+
+#include <glad/glad.h>
+
+#include "renderer.hpp"
+#include "util.hpp"
+
+DeferredRenderer::DeferredRenderer(EngineObject* parent) : EngineObject{"DeferredRenderer", parent} {}
+
+DeferredRenderer::~DeferredRenderer() = default;
+
+void DeferredRenderer::init(const unsigned int scrWidth, const unsigned int scrHeight)
+{
+    // create graphics buffer framebuffer
+    glGenFramebuffers(1, &m_gBuffer);
+    glBindFramebuffer(GL_FRAMEBUFFER, m_gBuffer);
+    // position color buffer
+    glGenTextures(1, &m_positionEBuffer);
+    glBindTexture(GL_TEXTURE_2D, m_positionEBuffer);
+    // NOTE: upgrade to GL_RGBA32F incase of precision artifacts
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA16F, scrWidth, scrHeight, 0, GL_RGBA, GL_FLOAT, nullptr);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, m_positionEBuffer, 0);
+    // albedo color buffer
+    glGenTextures(1, &m_colorBuffer);
+    glBindTexture(GL_TEXTURE_2D, m_colorBuffer);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, scrWidth, scrHeight, 0, GL_RGBA8, GL_UNSIGNED_BYTE, nullptr);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT1, GL_TEXTURE_2D, m_colorBuffer, 0);
+    // normal color buffer
+    glGenTextures(1, &m_normalEBuffer);
+    glBindTexture(GL_TEXTURE_2D, m_normalEBuffer);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA16F, scrWidth, scrHeight, 0, GL_RGBA16F, GL_FLOAT, nullptr);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT2, GL_TEXTURE_2D, m_normalEBuffer, 0);
+    // AMRE color buffer (AO, metallic, roughness, emissive)
+    glGenTextures(1, &m_AMREBuffer);
+    glBindTexture(GL_TEXTURE_2D, m_AMREBuffer);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, scrWidth, scrHeight, 0, GL_RGBA8, GL_UNSIGNED_BYTE, nullptr);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT3, GL_TEXTURE_2D, m_AMREBuffer, 0);
+
+    // add color attachments to framebuffer (let OpenGL know which ones to use for rendering)
+    unsigned int attachments[4]{GL_COLOR_ATTACHMENT0, GL_COLOR_ATTACHMENT1, GL_COLOR_ATTACHMENT2, GL_COLOR_ATTACHMENT2};
+    glDrawBuffers(4, attachments);
+
+    glGenRenderbuffers(1, &m_RBO);
+    glBindRenderbuffer(GL_RENDERBUFFER, m_RBO);
+    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT, scrWidth, scrHeight);
+    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, m_RBO);
+
+    // check framebuffer status
+    if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+    {
+	Util::beginError();
+	std::cout << "DEFERRED_RENDERER::INIT::ERROR: Framebuffer is not complete!" << std::endl;
+	Util::endError();
+    }
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+}
---
+++ src/renderer.hpp
@@ -0,0 +1,44 @@
+//
+// Created by jenskromdijk on 23/11/2025.
+//
+
+#ifndef MAIN_RENDERER_H
+#define MAIN_RENDERER_H
+
+#include "engine_types.hpp"
+
+class DeferredRenderer final : public EngineObject
+{
+public:
+    explicit DeferredRenderer(EngineObject* parent);
+    ~DeferredRenderer() override;
+
+    // setup framebuffers
+    void init(unsigned int scrWidth, unsigned int scrHeight);
+
+private:
+    // graphics buffer
+    unsigned int m_gBuffer{};
+    // positionEBuffer.xyz = Position
+    // positionEBuffer.w = Emissive r
+    unsigned int m_positionEBuffer{};
+    // colorBuffer.xyzw = rgba
+    unsigned int m_colorBuffer{};
+    // normalEBuffer.xyz = Normal
+    // normalEBuffer.w = Emissive.g
+    unsigned int m_normalEBuffer{};
+    // x = AO
+    // y = Roughness
+    // z = Metallic
+    // a = Emissive.b
+    unsigned int m_AMREBuffer{};
+    // render buffer
+    unsigned int m_RBO{};
+
+    // view port dimensions
+    unsigned int m_scrWidth{};
+    unsigned int m_scrHeight{};
+};
+
+
+#endif //MAIN_RENDERER_H
