f5a19fb66505172d4f377998d4679ea3563b7084
Jens Kromdijk <jenskromdijk@gmail.com>
2026-03-14 15:31:46 +0000

    Revert back to single thread

--- src/assets.hpp
+++ src/assets.hpp
@@ -4,8 +4,6 @@
 #define ASSETS_H
 
 #include <filesystem>
-#include <atomic>
-#include <thread>
 #include <algorithm>
 
 #include "core/engine.hpp"
@@ -14,22 +12,8 @@
 
 struct Assets
 {
-    explicit Assets() {}
-    ~Assets()
-    {
-        if (m_loader.joinable())
-            m_loader.join();
-    }
-
-    std::atomic<float> m_progress{0.0f};
-    std::atomic<bool> m_loaded{false};
-
-    std::thread m_loader{};
-
     unsigned int m_SFX_metalImpact;
     unsigned int m_MUSIC_menu;
-
-    std::vector<Model*> m_mugShards{};
 
     void loadFolder(std::vector<Model*>& models, const char* path, const std::string& name, Engine* engine, float start,
                     float end)
@@ -50,7 +34,6 @@ struct Assets
 
         if (modelPaths.empty())
         {
-            m_progress = end;
             return;
         }
 
@@ -62,33 +45,13 @@ struct Assets
                 engine->addModel(modelName, modelPaths[i].string());
             }
             models.emplace_back(engine->getModel(modelName));
-
-            m_progress = start + (static_cast<float>(i + 1) / static_cast<float>(modelPaths.size()) * (end - start));
         }
     }
 
     void loadAssets(AudioHandler* audioHandler, Engine* engine)
     {
-        if (m_loader.joinable())
-            m_loader.join();
-
-        m_loaded = false;
-        m_progress = 0.0f;
-
-        m_loader = std::thread(
-            [this, audioHandler, engine]()
-            {
-                m_SFX_metalImpact = audioHandler->loadSound("data/audio/sfx/metal_impact.ogg");
-                m_progress = 0.1f;
-
-                m_MUSIC_menu = audioHandler->loadStream("data/audio/music/menu.mp3");
-                m_progress = 0.2f;
-
-                loadFolder(m_mugShards, "data/models/mug_shards/", "mug_shards", engine, 0.2f, 1.0f);
-
-                m_progress = 1.0f;
-                m_loaded = true;
-            });
+        m_SFX_metalImpact = audioHandler->loadSound("data/audio/sfx/metal_impact.ogg");
+        m_MUSIC_menu = audioHandler->loadStream("data/audio/music/menu.mp3");
     }
 };
 
